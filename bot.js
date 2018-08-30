a61ccf863d059d903247e95e17b6892fb6f63bc6
James Gilchrist <jpgilchrist@pop-os.localdomain>
2018-08-30 12:43:03 -0400

    translate command

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js')
 const winston = require('winston')
 const auth = require('./auth.json')
+const translate = require('node-ermahgerd').translate;
 
 const logger = winston.createLogger({
     level: 'debug',
@@ -27,14 +28,13 @@ client.on('message', (message) => {
         const cmd = args[0]
         console.log('Received Command: ' + cmd)
         args = args.splice(1);
-        switch(cmd) {
-            case 'shuffle':
-            const _names = message.channel.members.map(m => m.username)
-            
-            console.log('Channel has members: ' + message.channel.members)
-            message.channel.send('shuffling...')
-                .then(sent => console.log(`Sent Message: ${sent.content}`))
-                .catch(console.error)        
+        args = args.join(' ');
+        switch(cmd.toLowerCase()) {
+            case 'translate':
+                const translation = translate(args);
+                message.channel.send(translation)
+                    .then(sent => console.log(`Sent Message: ${sent.content}`))
+                    .catch(console.error);
                 break;
         }
     }
